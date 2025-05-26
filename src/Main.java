642067f36f29ef4aa2a9e659bed0a93bfccda929
aguiar-dev-cs <franciscowederdasilvaaguiar@gmail.com>
2025-05-26 18:30:26 -0400

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -19,7 +21,36 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso);
-        System.out.println(mentoria);
+//        System.out.println(curso);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java");
+        bootcamp.setDescricao("Descrição Bootcamp Java");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAguiar = new Dev();
+        devAguiar.setNome("Aguiar");
+        devAguiar.inscreverBootcamp(bootcamp);
+        System.out.println("Contéudos inscritos Aguiar " + devAguiar.getConteudosInscritos());
+        devAguiar.progredir();
+        System.out.println("-");
+        System.out.println("Contéudos inscritos Aguiar " + devAguiar.getConteudosInscritos());
+        System.out.println("Contéudos concluídos Aguiar " + devAguiar.getConteudosConlcuidos());
+        System.out.println("Calcular XP: " + devAguiar.calcularTotalXp());
+
+        System.out.println("---------");
+
+        Dev devFrancisco = new Dev();
+        devFrancisco.setNome("Francisco");
+        devFrancisco.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Francisco " + devFrancisco.getConteudosInscritos());
+        devFrancisco.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Francisco " + devFrancisco.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Francisco " + devFrancisco.getConteudosConlcuidos());
+        System.out.println("Calcular XP: " + devFrancisco.calcularTotalXp());
+
     }
 }
